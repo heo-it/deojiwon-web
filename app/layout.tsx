96d035ea17398bba96b723a6341743238c9a4930
허예진 <yejin097@naver.com>
2025-04-13 14:15:09 +0900

    feat: pretendard local font 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import "./globals.css";
 
+import localFont from "next/font/local";
+
+const pretendard = localFont({
+  src: "../assets/fonts/PretendardVariable.woff2",
+  display: "swap",
+  weight: "45 920",
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={pretendard.className}>
       <body>{children}</body>
     </html>
   );
